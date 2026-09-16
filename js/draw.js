5a2e02d1695dc4e6083a75e61c19f07f93e94114
Rylan <rfay_msasd@magnoliascience.org>
2026-09-16 11:27:50 -0700

    SPIKES!!!

--- js/draw.js
+++ js/draw.js
@@ -21,7 +21,7 @@ Draw.updateCamera = function () {
 Draw.everything = function () {
   var ctx = Draw.ctx;
 
-  ctx.fillStyle = "#090403";
+  ctx.fillStyle = "#090303";
   ctx.fillRect(0, 0, CONFIG.CANVAS_W, CONFIG.CANVAS_H);
 
   ctx.save();
